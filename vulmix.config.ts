950e24f55185e45b5d50ea909c5a6d9d574c4513
Victor Ribeiro <devjvribeiro@gmail.com>
2024-11-21 11:41:17 -0300

    feat: added custom dist root directory

--- vulmix.config.ts
+++ vulmix.config.ts
@@ -2,5 +2,9 @@ import { defineVulmixConfig } from './.vulmix/utils/defineVulmixConfig'
 
 // See: https://vulmix.dev/guide/structure/vulmix.config.ts
 export default defineVulmixConfig({
-  
+  dirs: {
+    dist: {
+      root: 'vulmix-gh-pages',
+    },
+  },
 })
