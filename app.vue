98db6b97307c2c4451688230ab932cbd865b382c
Victor Ribeiro <devjvribeiro@gmail.com>
2024-11-21 17:51:49 -0300

    feat: images load test

--- app.vue
+++ app.vue
@@ -1,3 +1,26 @@
 <template>
-  <VulmixWelcome />
+  <div>
+    <img src="/vulmix-gh-pages/a.png" :style="{height: '100px'}" />
+    <img src="@assets/a.png" :style="{height: '100px'}" />
+
+    <div
+      :style="{
+        backgroundImage: 'url(/vulmix-gh-pages/a.png)',
+        backgroundSize: 'contain',
+        width: '100px',
+        height: '100px',
+      }"
+    ></div>
+
+    <div
+      :style="{
+        backgroundImage: `url(${require('@assets/a.png')})`,
+        backgroundSize: 'contain',
+        width: '100px',
+        height: '100px',
+      }"
+    ></div>
+
+    <div>TEEEEEEEEEEEEEEEEEEXT</div>
+  </div>
 </template>
